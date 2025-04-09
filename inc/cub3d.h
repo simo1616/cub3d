1a42dabce8bf3a5a3aebe0b2dcb9d3fa68f53366
simo1616 <simo1616@gmail.com>
2025-04-09 17:06:49 +0200

    separate mlx

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -8,9 +8,13 @@
 typedef struct s_game {
 	void	*mlx;
 	void	*win;
+	void	*img;
 	char	**map;
 	t_config config;
 } t_game;
 
+
+
+int			handle_mlx(t_game *game);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbendidi <mbendidi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 19:24:05 by mbendidi          #+#    #+#             */
-/*   Updated: 2025/04/09 16:42:41 by mbendidi         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:05:33 by mbendidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,29 @@ void	print_debug(t_game *game)
 	printf("-----------------------------------------\n");
 }
 
+int	handle_mlx(t_game *game)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+	{
+		printf("Erreur : Init mlx\n");
+		return (0);
+	}
+	game->win = mlx_new_window(game->mlx, 800, 600, "Cube3D");
+	if (!game->win)
+	{
+		printf("Erreur : new_window\n");
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+		return (0);
+	}
+
+	mlx_hook(game->win, 17, 0, close_window, game);
+	mlx_hook(game->win, 2, 1L<<0, key_press, game);
+	mlx_loop(game->mlx);
+	return(1);
+}
+
 int	main(int ac, char **av)
 {
 	t_game	game;
@@ -51,24 +74,8 @@ int	main(int ac, char **av)
 	if (ft_parse(&game, av[1]))
 		return (EXIT_FAILURE);
 	//print_debug(&game);
-	game.mlx = mlx_init();
-	if (!game.mlx)
-	{
-		printf("Erreur : Init mlx\n");
-		return (1);
-	}
-	game.win = mlx_new_window(game.mlx, 800, 600, "Cube3D");
-	if (!game.win)
-	{
-		printf("Erreur : new_window\n");
-		mlx_destroy_display(game.mlx);
-		free(game.mlx);
-		return (1);
-	}
-
-	mlx_hook(game.win, 17, 0, close_window, &game);
-	mlx_hook(game.win, 2, 1L<<0, key_press, &game);
-	mlx_loop(game.mlx);
+	if(handle_mlx(&game))
+		return (EXIT_FAILURE);
 	cleanup_before_exit(&game);
 	return (EXIT_SUCCESS);
 }
