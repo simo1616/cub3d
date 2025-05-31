6958556440559d86b2aba10dad0f8bcc8fdbe23f
simo1616 <simo1616@gmail.com>
2025-05-31 14:31:23 +0200

    norminette #15 parse_start_mlx

--- src/start_mlx.c
+++ src/start_mlx.c
@@ -6,55 +6,59 @@
 /*   By: mbendidi <mbendidi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:10:47 by mbendidi          #+#    #+#             */
-/*   Updated: 2025/05/31 13:38:12 by mbendidi         ###   ########.fr       */
+/*   Updated: 2025/05/31 14:30:15 by mbendidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/*
-** Initialise MLX, la fenêtre et l’image.
-** Retourne 1 si tout s'est bien passé, -1 sinon.
-*/
-static int	init_mlx_components(t_game *game)
+static int	init_mlx_ptr(t_game *game)
 {
-	char	*addr;
-
 	game->mlx = mlx_init();
 	if (!game->mlx)
 	{
-		printf("Erreur : Init mlx\n");
+		ft_putstr_fd("Error\nInit mlx\n", 2);
 		return (-1);
 	}
+	return (0);
+}
+
+static int	init_window(t_game *game)
+{
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "Cub3D");
 	if (!game->win)
 	{
-		printf("Erreur : new_window\n");
+		ft_putstr_fd("Error\nnew_window\n", 2);
 		mlx_destroy_display(game->mlx);
 		free(game->mlx);
 		return (-1);
 	}
-	game->data_img.img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
-	if (!game->data_img.img)
+	return (0);
+}
+
+static int	init_image(t_game *g)
+{
+	char	*addr;
+
+	g->data_img.img = mlx_new_image(g->mlx, WIDTH, HEIGHT);
+	if (!g->data_img.img)
 	{
-		printf("Erreur : new_img\n");
-		mlx_destroy_window(game->mlx, game->win);
-		mlx_destroy_display(game->mlx);
-		free(game->mlx);
+		ft_putstr_fd("Error\nnew_image\n", 2);
+		mlx_destroy_window(g->mlx, g->win);
+		mlx_destroy_display(g->mlx);
+		free(g->mlx);
 		return (-1);
 	}
-	addr = mlx_get_data_addr(game->data_img.img,
-			&game->data_img.bpp,
-			&game->data_img.size_line,
-			&game->data_img.endian);
-	game->data_img.pixels = (int *)addr;
-	game->data_img.size_line /= (game->data_img.bpp / 8);
-	return (1);
+	addr = mlx_get_data_addr(g->data_img.img,
+			&g->data_img.bpp, &g->data_img.size_line, &g->data_img.endian);
+	g->data_img.pixels = (int *)addr;
+	g->data_img.size_line /= (g->data_img.bpp / 8);
+	return (0);
 }
 
 int	start_mlx(t_game *game)
 {
-	if (init_mlx_components(game) == -1)
+	if (init_mlx_ptr(game) || init_window(game) || init_image(game))
 		return (-1);
 	game->player = get_coord(game);
 	text_load(game);
