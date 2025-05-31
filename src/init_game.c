c1e47964a3145f44de89b7806eadfa7aedca9f18
simo1616 <simo1616@gmail.com>
2025-05-31 14:37:23 +0200

    norminette #16 init_game

--- src/init_game.c
+++ src/init_game.c
@@ -6,65 +6,75 @@
 /*   By: mbendidi <mbendidi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:37:16 by mbendidi          #+#    #+#             */
-/*   Updated: 2025/05/31 13:39:56 by mbendidi         ###   ########.fr       */
+/*   Updated: 2025/05/31 14:36:20 by mbendidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/*
-** Initialise les textures à NULL ou 0.
-*/
-static void	init_textures(t_tex textures[NBR_TEX])
+static void	init_textures(t_tex text[NBR_TEX])
 {
 	int	i;
 
 	i = 0;
 	while (i < NBR_TEX)
 	{
-		textures[i].img = NULL;
-		textures[i].pixels = NULL;
-		textures[i].width = 0;
-		textures[i].height = 0;
-		textures[i].bpp = 0;
-		textures[i].size_line = 0;
-		textures[i].endian = 0;
+		text[i].img = NULL;
+		text[i].pixels = NULL;
+		text[i].width = 0;
+		text[i].height = 0;
+		text[i].bpp = 0;
+		text[i].size_line = 0;
+		text[i].endian = 0;
 		i++;
 	}
 }
 
-void	init_structs(t_game *game)
+static void	init_data_img(t_data_img *data_img)
+{
+	data_img->img = NULL;
+	data_img->pixels = NULL;
+	data_img->bpp = 0;
+	data_img->size_line = 0;
+	data_img->endian = 0;
+}
+
+static void	init_config(t_config *config)
 {
 	int	i;
 
+	config->no_texture = NULL;
+	config->so_texture = NULL;
+	config->we_texture = NULL;
+	config->ea_texture = NULL;
 	i = 0;
-	ft_memset(game, 0, sizeof(*game));
-	game->mlx = NULL;
-	game->win = NULL;
-	game->data_img.img = NULL;
-	game->data_img.pixels = NULL;
-	game->data_img.bpp = 0;
-	game->data_img.size_line = 0;
-	game->data_img.endian = 0;
-	game->config.no_texture = NULL;
-	game->config.so_texture = NULL;
-	game->config.we_texture = NULL;
-	game->config.ea_texture = NULL;
 	while (i < 3)
 	{
-		game->config.floor_color[i] = -1;
-		game->config.ceiling_color[i] = -1;
+		config->floor_color[i] = -1;
+		config->ceiling_color[i] = -1;
 		i++;
 	}
+}
+
+static void	init_player_defaults(t_player *player)
+{
+	player->x = WIDTH / 2;
+	player->y = HEIGHT / 2;
+	player->angle = -PI / 2;
+	player->key_up = false;
+	player->key_down = false;
+	player->key_left = false;
+	player->key_right = false;
+	player->left_rotate = false;
+	player->right_rotate = false;
+}
+
+void	init_structs(t_game *game)
+{
+	ft_memset(game, 0, sizeof(*game));
+	init_data_img(&game->data_img);
+	init_config(&game->config);
 	game->map = NULL;
-	game->player.x = WIDTH / 2;
-	game->player.y = HEIGHT / 2;
-	game->player.angle = -PI / 2;
-	game->player.key_up = false;
-	game->player.key_down = false;
-	game->player.key_left = false;
-	game->player.key_right = false;
-	game->player.left_rotate = false;
-	game->player.right_rotate = false;
+	init_player_defaults(&game->player);
 	init_textures(game->textures);
 }
